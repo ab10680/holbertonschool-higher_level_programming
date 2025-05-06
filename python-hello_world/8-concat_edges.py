b8372f1d4c7df41e698de6a2705adfa59982f9ff
ab10680 <10680@holbertonstudents.com>
2025-05-06 17:10:33 +0000

    task 6 - concat string slices to form sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming"
-str = str + " language that combines remarkable power with very clear syntax"
-# This is a filler comment to ensure 5 lines
-print(str[39:66] + " " + str[107:111] + " " + str[0:6])
+str = "Python is an interpreted, interactive, object-oriented programming" \
+          " language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[0:6])
