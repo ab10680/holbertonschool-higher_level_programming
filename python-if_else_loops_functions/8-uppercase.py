8216eabd4aad307d00e455627474a24e6a44c45d
ab10680 <10680@holbertonstudents.com>
2025-05-08 17:09:09 +0000

    task 8 - print string in uppercase using ord() - 2

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    """Print a string in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:c}".format(ord(c)), end="")
-    print()
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
