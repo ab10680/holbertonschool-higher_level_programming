3d263f90d048dcf3d0db412b9456ccab6da7703a
ab10680 <10680@holbertonstudents.com>
2025-06-02 17:14:05 +0000

    Task 04 - Convert JSON string to object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Converts a JSON string to a Python object.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the Python object representation of a JSON string."""
+    return json.loads(my_str)
