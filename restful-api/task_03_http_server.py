e9ba3178fc6169d60e555c6d6e75f7d69ebc43da
ab10680 <10680@holbertonstudents.com>
2025-06-21 04:06:45 +0000

    Task 3 - Final version passed all tests

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,42 +4,35 @@ import http.server
 import socketserver
 import json
 
-
 PORT = 8000
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
+    def _set_headers(self, code=200, content_type="application/json"):
+        self.send_response(code)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
     def do_GET(self):
         if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
+            self._set_headers(200, "text/plain")
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
+            self._set_headers()
             response_data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
             }
             self.wfile.write(json.dumps(response_data).encode('utf-8'))
+
         elif self.path == "/status":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            """response_data = {
-                "status": "OK"
-            }"""
-            self.wfile.write(b"OK")
+            self._set_headers()
+            self.wfile.write(b'{"status":"OK"}')
+
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            """response_data = {
-                "error": "Endpoint not found"
-            }"""
-            self.wfile.write(b"404 Not Found")
+            self._set_headers(404)
+            self.wfile.write(b'{"error":"Endpoint not found"}')
 
 Handler = MyHandler
 
