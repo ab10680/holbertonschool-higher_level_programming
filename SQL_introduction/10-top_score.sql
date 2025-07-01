f4007417b2ac9ab713c83ed61b2a11d39fba4f05
ab10680 <10680@holbertonstudents.com>
2025-07-01 15:01:19 +0000

    task 10 - List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Task 10: List all records ordered by score descending
+-- This script selects score and name from second_table and orders by score descending
+
+SELECT score, name FROM second_table
+ORDER BY score DESC;
