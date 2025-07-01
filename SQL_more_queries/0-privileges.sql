02e236f62fd442f618ae8615064d9e9cdf8e17da
ab10680 <10680@holbertonstudents.com>
2025-07-01 15:32:11 +0000

    task 0 - My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Task 0: List privileges for user_0d_1 and user_0d_2
+-- This script shows all privileges for the specified users
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
