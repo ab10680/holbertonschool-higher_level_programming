732068362c1e3b000398755f2d9c255317fb23f0
ab10680 <10680@holbertonstudents.com>
2025-07-12 19:58:15 +0000

    task 10 - Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (isNaN(n) || n < 0) {
+// computes and prints a factorial
+
+function factorial (n) {
+  if ((isNaN(n)) || (n === 1)) {
     return 1;
+  } else {
+    return n * factorial(n - 1);
   }
-  if (n === 0) {
-    return 1;
-  }
-  return n * factorial(n - 1);
 }
 
-const num = parseInt(process.argv[2]);
-console.log(factorial(num));
+console.log(factorial(parseInt(process.argv[2])));
