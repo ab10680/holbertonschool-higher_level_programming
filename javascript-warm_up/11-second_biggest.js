86ed33e86b27c853476992bce239a9362d14ed04
ab10680 <10680@holbertonstudents.com>
2025-07-12 20:01:29 +0000

    task 11 - Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
+const args = process.argv.slice(2).map(Number);
 
-if (process.argv.length <= 3) {
+if (args.length < 2) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
