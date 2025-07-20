8f2edd880a4b0df51edd25ff711f77d4240b2f04
ab10680 <10680@holbertonstudents.com>
2025-07-20 15:05:34 +0000

    task 3 - Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleHeader = document.querySelector('#toggle_header');
+  const header = document.querySelector('header');
+
+  toggleHeader.addEventListener('click', () => {
+    if (header.classList.contains('red')) {
+      header.classList.replace('red', 'green');
+    } else if (header.classList.contains('green')) {
+      header.classList.replace('green', 'red');
+    }
+  });
+});
